a9039f1bec2d9e38c03a09d1d5298141a34d9aef
Scott Wilson <apollo@Scotts-MBP.attlocal.net>
2019-06-04 11:13:42 -0700

    styling features

--- src/components/HomePageComponents/Feature.js
+++ src/components/HomePageComponents/Feature.js
@@ -2,16 +2,48 @@ import React from 'react'
 import styled from 'styled-components'
 import { styles } from '../../utils'
 import Img from 'gatsby-image'
+import tw from 'tailwind.macro'
 
 export default function Feature({ feature }) {
   const { name } = feature
   const { fixed } = feature.image
   return (
     <FeatureWrapper>
-      <Img fixed={fixed} />
-      <p>{name}</p>
+      <Card>
+        <div className="icon">
+          <Img fixed={fixed} />
+        </div>
+        <div className="text">{name}</div>
+      </Card>
     </FeatureWrapper>
   )
 }
 
 const FeatureWrapper = styled.div``
+
+const Card = styled.div`
+  ${tw`p-4 m-4 md:m-6 shadow-md rounded`}
+
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-column-gap: 1rem;
+
+  background-color: #dcdfe2;
+  color: black;
+  transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  &:hover {
+    transform: translateY(-5px);
+  }
+
+  .text {
+    display: flex;
+    align-items: center;
+    @media (min-width: 600px) {
+      font-size: 1.2rem;
+    }
+
+    @media (min-width: 1000px) {
+      justify-content: space-evenly;
+    }
+  }
+`
