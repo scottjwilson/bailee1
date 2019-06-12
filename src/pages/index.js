9121b3afe5e698052ff9afbfde6a85f7f1d87199
Scott Wilson <apollo@Scotts-MBP.attlocal.net>
2019-06-12 08:58:42 -0700

    removed all css directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,19 +10,12 @@ import { Title } from '../utils'
 
 import img from '../images/bcg/homeBcg.jpeg'
 
-import tw from 'tailwind.macro'
-
 //Sections
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
 import Form from '../components/HomePageComponents/Form'
 import FeaturedBusinesses1 from '../components/HomePageComponents/FeaturedBusinesses1'
-import FeaturedBusinesses from '../components/HomePageComponents/FeaturedBusinesses'
-import AppFeatures from '../components/HomePageComponents/AppFeatures'
 
-import avatar1 from '../images/avatar1.jpg'
-import avatar2 from '../images/avatar2.png'
-import avatar3 from '../images/avatar3.jpg'
-import avatar4 from '../images/avatar4.jpg'
+import AppFeatures from '../components/HomePageComponents/AppFeatures'
 
 import {
   mainstyles,
@@ -53,58 +46,6 @@ class RootIndex extends React.Component {
         </HomeHeader>
         <QuickInfo />
         <FeaturedBusinesses1 />
-        <FeaturedBusinesses>
-          <Title title="Featured Businesses" />
-          <BusinessCard
-            title="Frenchie Lamont"
-            link="#"
-            bg={mainstyles.gradients.redGrad}
-            text="test"
-          >
-            <Avatar src={avatar3} alt="Frenchie Lamont" />
-            <BusinessDescription>
-              A graphic designer that offers high quality custom t-shirts. I
-              also create logos for your personal and business needs
-            </BusinessDescription>
-          </BusinessCard>
-          <BusinessCard
-            title="One Love Custom Apparel"
-            link="#"
-            bg={mainstyles.gradients.blackGrad}
-            text="test"
-          >
-            <Avatar src={avatar1} alt="One Love Custom Apparel" />
-            <BusinessDescription>
-              Your premier full service custom t-shirt screen priting provider.
-              We also printer on mugs, hats, and shot glasses.
-            </BusinessDescription>
-          </BusinessCard>
-          <BusinessCard
-            title="Yeyo 2 Yaya"
-            link="#"
-            bg={mainstyles.gradients.redGrad}
-            text="test"
-          >
-            <Avatar src={avatar4} alt="Yeyo 2 Yaya" />
-            <BusinessDescription>
-              Birth and postpartum Doula services, womb stimulation, placenta
-              encapsulation
-            </BusinessDescription>
-          </BusinessCard>
-          <BusinessCard
-            title="Martous Naturals"
-            link="#"
-            bg={mainstyles.gradients.blackGrad}
-            text="test"
-          >
-            <Avatar src={avatar2} alt="Martous Naturals" />
-            <BusinessDescription>
-              A family owned natural products company located in Atlanta,
-              Georgia. Our products include handmade soaps, lotions, and sugar
-              scrubs for women and men.
-            </BusinessDescription>
-          </BusinessCard>
-        </FeaturedBusinesses>
 
         <AppFeatures />
         <Form />
@@ -112,13 +53,6 @@ class RootIndex extends React.Component {
     )
   }
 }
-
-const BusinessDescription = styled.p`
-  ${tw`py-6`}
-`
-const Avatar = styled.img`
-  ${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto`};
-`
 
 const Download = styled.a`
   color: white;
