b1859e4a623531afb0fa1c9560d21061b712d2e0
Scott Wilson <apollo@Scotts-MBP.attlocal.net>
2019-06-23 13:22:12 -0700

    updated footer

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -1,16 +1,8 @@
-import React, { Component } from 'react'
-import SEO from '../components/SEO'
+import React from 'react'
 import Layout from '../components/layout'
 
-export default class terms extends Component {
-  render() {
-    return (
-      <Layout>
-        <SEO
-          title="Privacy Statement"
-          description="best black owned business directory bailee businesses"
-        />
-      </Layout>
-    )
-  }
+const terms = () => {
+  return <Layout>hi from terms</Layout>
 }
+
+export default terms
