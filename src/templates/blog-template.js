f62688809e76c91bc4a856a1fe4aa1c84afd666a
Scott Wilson <apollo@Scotts-MBP.attlocal.net>
2019-07-27 18:02:44 -0700

    testing share icons 2

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -7,7 +7,20 @@ import blogstyle from './blog-template.module.css'
 import SEO from '../components/SEO'
 import { DiscussionEmbed } from 'disqus-react'
 
-import { FacebookShareCount } from 'react-share'
+import {
+  FacebookIcon,
+  TwitterIcon,
+  PinterestIcon,
+  RedditIcon,
+  TumblrIcon,
+  EmailIcon,
+  FacebookShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  EmailShareButton,
+} from 'react-share'
 
 const Blog = ({ data, pageContext }) => {
   const {
@@ -45,11 +58,44 @@ const Blog = ({ data, pageContext }) => {
   return (
     <Layout>
       <SEO title={title} />
+
       <section className={blogstyle.blog}>
+        <div className={blogstyle.socialbar}>
+          <div className={blogstyle.social}>
+            <FacebookShareButton url={shareCount.url}>
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+          </div>
+          <div className={blogstyle.social}>
+            <TwitterShareButton url={shareCount.url}>
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+          </div>
+          <div className={blogstyle.social}>
+            <PinterestShareButton url={shareCount.url}>
+              <PinterestIcon size={32} round={true} />
+            </PinterestShareButton>
+          </div>
+          <div className={blogstyle.social}>
+            <RedditShareButton url={shareCount.url}>
+              <RedditIcon size={32} round={true} />
+            </RedditShareButton>
+          </div>
+          <div className={blogstyle.social}>
+            <TumblrShareButton url={shareCount.url}>
+              <TumblrIcon size={32} round={true} />
+            </TumblrShareButton>
+          </div>
+          <div className={blogstyle.social}>
+            <EmailShareButton url={shareCount.url}>
+              <EmailIcon size={32} round={true} />
+            </EmailShareButton>
+          </div>
+        </div>
         <div className={blogstyle.center}>
           <h1>{title}</h1>
           <h4>Published at : {publishDate}</h4>
-          <FacebookShareCount url={shareCount.url} />
+
           <article className={blogstyle.post}>
             {documentToReactComponents(json, options)}
           </article>
