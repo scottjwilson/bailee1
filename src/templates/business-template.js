fd3a915e75b935d2692cf479516949889f7dcf05
Scott <slimzel@protonmail.com>
2019-10-02 12:10:31 -0700

    fixed padding on biz template

--- src/templates/business-template.js
+++ src/templates/business-template.js
@@ -17,7 +17,7 @@ const Template = ({ data }) => {
       <SEO title={name} />
       <Hero img={mainImage.fluid} />
       <section className={businesstemplate.template}>
-        <div className={businesstemplate.center}>
+        <div className={businesstemplate.content}>
           <h1>{name}</h1>
           <div className={businesstemplate.info}>
             <p>{category}</p>
@@ -26,36 +26,37 @@ const Template = ({ data }) => {
               {location}
             </p>
           </div>
-
-          <p className={businesstemplate.desc}>{description}</p>
+          <div className={businesstemplate.descContainer}>
+            <p className={businesstemplate.desc}>{description}</p>
+          </div>
 
           <div className={businesstemplate.linkwrapper}>
             <div className={businesstemplate.btnprimary}>
               <a href={link}>Visit Business</a>
             </div>
           </div>
+        </div>
 
-          <div className={businesstemplate.images}>
-            {allImages.map((item, index) => {
-              return (
-                <Img
-                  key={index}
-                  fluid={item.fluid}
-                  alt="single tour"
-                  className={businesstemplate.image}
-                />
-              )
-            })}
-          </div>
-          <div className={businesstemplate.btnwrapper}>
-            <AniLink
-              fade
-              to="/businesses"
-              className={businesstemplate.btnprimary}
-            >
-              back to businesses
-            </AniLink>
-          </div>
+        <div className={businesstemplate.images}>
+          {allImages.map((item, index) => {
+            return (
+              <Img
+                key={index}
+                fluid={item.fluid}
+                alt="single tour"
+                className={businesstemplate.image}
+              />
+            )
+          })}
+        </div>
+        <div className={businesstemplate.btnwrapper}>
+          <AniLink
+            fade
+            to="/businesses"
+            className={businesstemplate.btnprimary}
+          >
+            back to businesses
+          </AniLink>
         </div>
       </section>
     </Layout>
