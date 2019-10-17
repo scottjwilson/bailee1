908dd3ab6697e6d4145f0780fcc0fa14a13f792b
Scott <hey@scottwil.com>
2019-10-17 09:51:59 -0700

    testing context on build

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import styles from '../css/navbar.module.css'
+import React, { Component } from 'react'
+import styles from '.navbar.module.css'
 import Image from './Image'
 
-import { AppContext } from '../context'
-const Navbar = () => {
-  const { handleOpenSidebar } = React.useContext(AppContext)
-  return (
-    <nav>
-      <button
-        type="button"
-        className={styles.toggleBtn}
-        onClick={handleOpenSidebar}
-      >
-        menu
-        <Image />
-      </button>
-    </nav>
-  )
-}
+export default class Navbar extends Component {
+  state = {
+    toggle: true,
+  }
 
-export default Navbar
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle,
+    })
+  }
+
+  render() {
+    return (
+      <nav>
+        <button
+          type="button"
+          className={styles.toggleBtn}
+          onClick={this.toggle}
+        >
+          menu
+          <Image />
+        </button>
+      </nav>
+    )
+  }
+}
